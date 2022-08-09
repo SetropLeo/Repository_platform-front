4e525102e4fe0f611596f8534b0983829270c603
LeonardoPortes <leol_portes@hotmail.com>
2022-08-09 20:41:25 -0300

    Create the LoginPage component and apply its styles

---
+++ src/pages/LoginPage/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './styles.css';
+
+const LoginPage = () => {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleLogin = () => {
+    console.log('email', email);
+    console.log('password', password);
+  };
+
+  return (
+    <div id="login">
+      <h1 className="title">Login</h1>
+      <div className="form">
+        <div className="field">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div className="field">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <div className="action">
+          <button onClick={handleLogin}>Entrar</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
