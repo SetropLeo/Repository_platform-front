ec0ffad0a1329c833b953fbb9d51bfc4cbb3d7d5
LeonardoPortes <leol_portes@hotmail.com>
2022-08-29 19:20:08 -0300

    Fix RepoList Component

--- src/pages/MainPage/RepoList/RepoList.jsx
+++ src/pages/MainPage/RepoList/RepoList.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 
 const RepoList = ({ repositories, onDelete }) => {
-  
+
+  const filterName = (name) => {
+    return name.substring(0, name.indexOf('/'));
+  }
+
+  const filterOwner = (name) => {
+    return name.substring(name.indexOf('/') + 1);
+  }
+
   return (
     <ul className="list">
-      <li className="item">
-        <div className="info">
-          <div className="owner">Facebook</div>
-          <div className="name">React</div>
-        </div>
-        <button onClick={() => onDelete(null)}>Apagar</button>
-      </li>
+      {repositories && repositories.map((repository) => (
+        <li className="item" key={repository._id}>
+          <div className="info">
+            <div className="owner">{filterName(repository.name)}</div>
+            <div className="name">{filterOwner(repository.name)}</div>
+          </div>
+          <button onClick={() => onDelete(null)}>Apagar</button>
+        </li>
+      ))}
     </ul>
   );
 };
